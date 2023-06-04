f94b9c40420239930b3f61c00be3d1e2a0ecb743
Pknatic <info.pkrajput@gmail.com>
2023-06-04 23:46:57 +0530

    Initial commit

---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+class HelloWorld { // calss A big Group that holds all the function 
+    public static void main(String[] args) { // a function that holds function's contents 
+        System.out.println("Hello, World!"); 
+        System.out.println();
+    }
+}
