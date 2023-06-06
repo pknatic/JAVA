2ce8f92db47ecf75a6db326bc181fc9601ca9f83
Pknatic <info.pkrajput@gmail.com>
2023-06-06 13:43:40 +0530

    a

---
+++ First learn/learn/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Pknatic");
+        String pk = "I am a coder!";
+        System.out.println(pk);
+        int c = pk.length();
+        System.out.println(c);
+
+    }
+}
