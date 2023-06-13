3a3f5981d0d6693ffc6cd1aacc55d114fc2980fa
Pknatic <info.pkrajput@gmail.com>
2023-06-13 15:16:06 +0530

    update

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
-class HelloWorld { // calss A big Group that holds all the function 
-    public static void main(String[] args) { // a function that holds function's contents 
-        System.out.println("Hello, World!"); 
-        System.out.println();
+import java.util.Scanner;
+public class Helloworld {
+    public static void main(String[] args) {
+     System.out.println("Hello World !!");   
     }
+
 }
